e30867a99b757afbbe8d88c725769ea24b294a19
Harshkumar Gupta <harshwantstolearncode@gmail.com>
2024-03-27 16:57:11 +0530

    fifth problem of the day 27 march

---
+++ A_Ambitious_Kid.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define endl "\n" 
+void solve(){
+    int n;
+    cin>>n;
+    int ans=INT_MAX;
+    for(int i=0;i<n;i++){
+        int temp;
+        cin>>temp;
+        ans=min(ans,abs(temp));
+    }
+    cout<<ans<<endl;
+     return;
+}
+int main(void){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    ll testcase=1;
+    // cin>>testcase;
+    while(testcase--){
+         solve();
+    }
+    
+    return 0;
+}
