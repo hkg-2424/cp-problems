34caa711b87688c691d39dd30337f8171aa65f09
Harshkumar Gupta <harshwantstolearncode@gmail.com>
2024-03-29 01:56:22 +0530

    some problems from cp31-800 sheet

---
+++ A_Twin_Permutations.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define endl "\n" 
+void solve(){
+    int n;
+    cin>>n;
+    int arr[n];
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    for(int i=0;i<n;i++){
+        cout<<n+1-arr[i]<<' ';
+    }
+    cout<<endl;
+    return;
+}
+int main(void){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    ll testcase=1;
+    cin>>testcase;
+    while(testcase--){
+         solve();
+    }
+    
+    return 0;
+}
--- A_Unit_Array.cpp
+++ A_Unit_Array.cpp
@@ -3,7 +3,27 @@ using namespace std;
 #define ll long long
 #define endl "\n" 
 void solve(){
+    int n;
+    cin>>n;
+    int ct=0;
+    for(int i=0;i<n;i++){
+        int temp;
+        cin>>temp;
+        if(temp==-1)ct++;
+    }
+    if((n-ct)  >= ct){
+        //sum>=0
+        if(ct&1)cout<<1<<endl; // product is -1
+        else cout<<0<<endl;
 
+    }else{
+        //now sum is negative and is n-2*ct;
+        int ans=(2*ct-n)/2 + ((2*ct-n)&1);
+        if((ct-ans)&1)cout<<ans+1<<endl;
+        else cout<<ans<<endl;
+
+
+    }
 }
 int main(void){
     ios_base::sync_with_stdio(false);
